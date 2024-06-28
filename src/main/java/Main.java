9f7e3b4bb86df8e4f825e8eccc977b176de28a1a
nedmv <nedmvl@rambler.ru>
2024-06-28 23:55:33 +0300

    feat(java): unify load, solve, load_task methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,31 +9,54 @@ import java.nio.file.Paths;
 import parsing.Parser;
 import submittor.Submittor;
 
+
 public class Main {
-    static final String lambdaPath = "inputs/lambdaman/";
-    static final String spaceshipPath = "inputs/spaceship/";
+    static final String lambda = "lambdaman";
+    static final String space = "spaceship";
+    static final String d3 = "3d";
+
+    static final String tasksPath = "tasks/";
+    static final String inputsPath = "inputs/";
+    static final String outputsPath = "outputs/";
+
+    static final int maxLambdaman = 21;
+    static final int maxSpaceship = 25;
+    static final int max3d = 12;
     public static void main(String[] args) throws IOException, InterruptedException {
-        /*try (BufferedReader bw = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get("input"))))) {
-            Parser.parse(bw.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }        */
-        //String s = "S" + Parser.convertString("get spaceship1");
-        //System.out.println(s);
-        //Parser.parse(s);
+        load("3d", 1, max3d);
+        // load_task("lambdaman");
+        // load_task("spaceship");
+        // solve("lambdaman", 1, 5);
+        // solve("spaceship", 1, 10);
+        // load("spaceship", 1, maxSpaceship);
+        // load("lambdaman", 1, maxLambdaman);
 
-        //String res = Submittor.submit(s);
-        //Parser.parse(res);
-
-        //loadLambdaman();
-        loadSpaceship();
-
+        // String s = "S'%4}).$%8";
+        // String in = "";
+        // String s = "S" + Parser.convertString(in);
+        // String res = Submittor.submit(s);
+        // Parser.parse(res);
     }
 
-    public static void loadLambdaman() throws InterruptedException{
-        for (int i = 1; i <= 21; ++i) {
-            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(Paths.get(lambdaPath + i))))) {
-                String s = "S" + Parser.convertString("get lambdaman"+ i);                
+    public static void solve(String name, int start, int end) throws InterruptedException{
+        for (int i = start; i <= end; ++i) {
+            try (BufferedReader bw = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(outputsPath + name + "/" + i))))) {
+                String in = bw.readLine();
+                String out = "S" + Parser.convertString("solve " + name + i + " " + in);
+                String res = Submittor.submit(out);
+                Parser.parse(res);
+                System.out.println("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Thread.sleep(100);
+        }
+    }
+
+    public static void load(String name, int start, int end) throws InterruptedException{
+        for (int i = start; i <= end; ++i) {
+            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(Paths.get(inputsPath + name + "/" + i))))) {
+                String s = "S" + Parser.convertString("get " + name + i);                
                 String res = Submittor.submit(s);                
                 bw.write(Parser.parseString(res));
             } catch (IOException e) {
@@ -43,16 +66,13 @@ public class Main {
         }
     }
 
-    public static void loadSpaceship() throws InterruptedException{
-        for (int i = 1; i <= 25; ++i) {
-            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(Paths.get(spaceshipPath + i))))) {
-                String s = "S" + Parser.convertString("get spaceship"+ i);                
-                String res = Submittor.submit(s);                
-                bw.write(Parser.parseString(res));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Thread.sleep(100);
+    public static void load_task(String name) throws InterruptedException{
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(Paths.get(tasksPath + name + ".md"))))) {
+            String s = "S" + Parser.convertString("get " + name);                
+            String res = Submittor.submit(s);                
+            bw.write(Parser.parseString(res));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
